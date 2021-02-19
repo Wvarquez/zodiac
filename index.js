90c1baad2240526532c083c90599852ab257d92e
Whilce Raphael Raphael <wvarquez@hawaii.edu>
2021-02-19 00:35:24 +0000

    Added myZodiacSign3

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const zodiacSign = require('get-zodiac-sign');
 var myZodiacSign1 = zodiacSign(3,13);
 var myZodiacSign2 = zodiacSign(2,18);
+var myZodiacSign3 = zodiacSign(1, 29);
 console.log(myZodiacSign1);
 console.log(myZodiacSign2);
-//kgj;kjak j
+console.log(myZodiacSign3);
 
 //use the fs function that comes with node
 const fs = require("fs");
